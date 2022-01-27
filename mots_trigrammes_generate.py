f750b22e7be744f9fa2cf8f4df90f626c33099c0
hyagoro <steve.gabet@gmail.com>
2022-01-27 12:37:40 +0100

    (upgrade) avoid generate duplicates, add np seed

--- mots_trigrammes_generate.py
+++ mots_trigrammes_generate.py
@@ -8,6 +8,9 @@ import numpy as np
 from numpy.random import choice
 import codecs
 
+# For reproducibility purposes
+np.random.seed(42)
+
 # Build a dictionary to check whether word already exists
 filepath = "names.txt"
 dictionary = []
@@ -26,6 +29,8 @@ markov_chain[np.isnan(markov_chain)] = 0
 outfile = "output.txt"
 f = codecs.open(outfile, "w", "utf-8")
 
+already_generated = []
+
 # How many words for each target size
 nb_words = 100
 for word_size in range(4, 11):
@@ -39,13 +44,14 @@ for word_size in range(4, 11):
             unicode_word = unicode_word + chr(unicode_code)
             i = j
             j = unicode_code
-        if len(unicode_word) == 1 + word_size:
+        if len(unicode_word) == 1 + word_size and unicode_word[:-1] not in already_generated:
             # if the word exists in the dictionary, suffix it by *
             if unicode_word[:-1] in dictionary:
-                x = unicode_word[:-1] + "*"
+                final_word = unicode_word[:-1] + "*"
             else:
-                x = unicode_word[:-1]
+                final_word = unicode_word[:-1]
             generated_words += 1
-            print(x)
-            f.write(x + "\n")
+            already_generated.append(unicode_word[:-1])
+            print(final_word)
+            f.write(final_word + "\n")
 f.close()
