311ecb3b234033a921ab9219dd5a3b0f82094d4d
hyagoro <steve.gabet@gmail.com>
2022-01-27 12:25:37 +0100

    (upgrade) rename variables

--- mots_trigrammes_generate.py
+++ mots_trigrammes_generate.py
@@ -8,43 +8,44 @@ import numpy as np
 from numpy.random import choice
 import codecs
 
-# Build a dictionnary to check whether word already exists
+# Build a dictionary to check whether word already exists
 filepath = "names.txt"
-dico = []
+dictionary = []
 with codecs.open(filepath, "r", "utf-8") as lines:
-    for l in lines:
-        dico.append(l[:-1])
+    for line in lines:
+        dictionary.append(line[:-1])
 
-# Load the trigram count matrix and normalize it     
+# Load the trigram count matrix and normalize it (to be used as probability)
 count = np.fromfile("count.bin", dtype="int32").reshape(256, 256, 256)
 s = count.sum(axis=2)
 st = np.tile(s.T, (256, 1, 1)).T
-p = count.astype('float') / st
-p[np.isnan(p)] = 0
+markov_chain = count.astype('float') / st
+markov_chain[np.isnan(markov_chain)] = 0
 
 # Build words
 outfile = "output.txt"
 f = codecs.open(outfile, "w", "utf-8")
 
-# How many for each target size
-K = 100
-for TGT in range(4, 11):
-    total = 0
-    while total < 100:
+# How many words for each target size
+nb_words = 100
+for word_size in range(4, 11):
+    generated_words = 0
+    while generated_words < nb_words:
         i = 0
         j = 0
-        res = u''
+        unicode_word = u''
         while not j == 10:
-            k = choice(range(256), 1, p=p[i, j, :])[0]
-            res = res + chr(k)
+            unicode_code = choice(range(256), 1, p=markov_chain[i, j, :])[0]
+            unicode_word = unicode_word + chr(unicode_code)
             i = j
-            j = k
-        if len(res) == 1 + TGT:
-            if res[:-1] in dico:
-                x = res[:-1] + "*"
+            j = unicode_code
+        if len(unicode_word) == 1 + word_size:
+            # if the word exists in the dictionary, suffix it by *
+            if unicode_word[:-1] in dictionary:
+                x = unicode_word[:-1] + "*"
             else:
-                x = res[:-1]
-            total += 1
+                x = unicode_word[:-1]
+            generated_words += 1
             print(x)
             f.write(x + "\n")
 f.close()
