115b5016fa2dd7d8d92b08843e992970aa2372ae
hyagoro <steve.gabet@gmail.com>
2022-01-27 11:58:01 +0100

    (typo) reformat code

--- mots_trigrammes_count.py
+++ mots_trigrammes_count.py
@@ -7,18 +7,17 @@ import numpy as np
 
 import codecs
 
+filepath = "names.txt"
 
-filepath = "liste.de.mots.francais.frgut.txt"
-
-count = np.zeros((256,256,256),dtype='int32')
+count = np.zeros((256, 256, 256), dtype='int32')
 res = []
 
 with codecs.open(filepath, "r", "utf-8") as lines:
-    for l in  lines:
-        i=0
-        j=0
+    for l in lines:
+        i = 0
+        j = 0
         for k in [ord(c) for c in list(l)]:
-            count[i,j,k] += 1
+            count[i, j, k] += 1
             i = j
             j = k
 count.tofile("count.bin")
--- mots_trigrammes_generate.py
+++ mots_trigrammes_generate.py
@@ -9,43 +9,42 @@ from numpy.random import choice
 import codecs
 
 # Build a dictionnary to check whether word already exists
-filepath = "liste.de.mots.francais.frgut.txt"
+filepath = "names.txt"
 dico = []
 with codecs.open(filepath, "r", "utf-8") as lines:
-    for l in  lines:
+    for l in lines:
         dico.append(l[:-1])
- 
-# Load the trigram count matrixand normalize it     
-count = np.fromfile("count2D.bin",dtype="int32").reshape(256,256,256)
-s=count.sum(axis=2)
-st=np.tile(s.T,(256,1,1)).T
-p=count.astype('float')/st
-p[np.isnan(p)]=0
 
+# Load the trigram count matrix and normalize it     
+count = np.fromfile("count.bin", dtype="int32").reshape(256, 256, 256)
+s = count.sum(axis=2)
+st = np.tile(s.T, (256, 1, 1)).T
+p = count.astype('float') / st
+p[np.isnan(p)] = 0
 
 # Build words
 outfile = "output.txt"
-f = codecs.open(outfile,"w","utf-8")
+f = codecs.open(outfile, "w", "utf-8")
 
 # How many for each target size
 K = 100
-for TGT in range(4,11):
+for TGT in range(4, 11):
     total = 0
-    while total<100:
-        i=0
-        j=0
+    while total < 100:
+        i = 0
+        j = 0
         res = u''
-        while not j==10:
-            k=choice(range(256),1,p=p[i,j,:])[0]
+        while not j == 10:
+            k = choice(range(256), 1, p=p[i, j, :])[0]
             res = res + chr(k)
-            i=j
-            j=k
-        if len(res) == 1+TGT:
+            i = j
+            j = k
+        if len(res) == 1 + TGT:
             if res[:-1] in dico:
-                x=res[:-1]+"*"
+                x = res[:-1] + "*"
             else:
-                x=res[:-1]
+                x = res[:-1]
             total += 1
             print(x)
-            f.write(x+"\n")
+            f.write(x + "\n")
 f.close()
