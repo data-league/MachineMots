55eb97f93636a9580dab63a5cb1b70b9a971a8ea
hyagoro <steve.gabet@gmail.com>
2022-01-27 12:39:41 +0100

    (upgrade) add main

--- mots_trigrammes_count.py
+++ mots_trigrammes_count.py
@@ -1,23 +1,29 @@
 # -*- coding: utf-8 -*-
 
-# Count the occurence of ASCII characters trigrams in a text file
+# Count the occurrence of ASCII characters trigrams in a text file
 
 import os
 import numpy as np
 
 import codecs
 
-filepath = "names.txt"
 
-count = np.zeros((256, 256, 256), dtype='int32')
-res = []
+def run():
+    filepath = "names.txt"
 
-with codecs.open(filepath, "r", "utf-8") as lines:
-    for l in lines:
-        i = 0
-        j = 0
-        for k in [ord(c) for c in list(l)]:
-            count[i, j, k] += 1
-            i = j
-            j = k
-count.tofile("count.bin")
+    count = np.zeros((256, 256, 256), dtype='int32')
+    res = []
+
+    with codecs.open(filepath, "r", "utf-8") as lines:
+        for line in lines:
+            i = 0
+            j = 0
+            for k in [ord(c) for c in list(line)]:
+                count[i, j, k] += 1
+                i = j
+                j = k
+    count.tofile("count.bin")
+
+
+if __name__ == '__main__':
+    run()
--- mots_trigrammes_generate.py
+++ mots_trigrammes_generate.py
@@ -11,47 +11,53 @@ import codecs
 # For reproducibility purposes
 np.random.seed(42)
 
-# Build a dictionary to check whether word already exists
-filepath = "names.txt"
-dictionary = []
-with codecs.open(filepath, "r", "utf-8") as lines:
-    for line in lines:
-        dictionary.append(line[:-1])
 
-# Load the trigram count matrix and normalize it (to be used as probability)
-count = np.fromfile("count.bin", dtype="int32").reshape(256, 256, 256)
-s = count.sum(axis=2)
-st = np.tile(s.T, (256, 1, 1)).T
-markov_chain = count.astype('float') / st
-markov_chain[np.isnan(markov_chain)] = 0
+def run():
+    # Build a dictionary to check whether word already exists
+    filepath = "names.txt"
+    dictionary = []
+    with codecs.open(filepath, "r", "utf-8") as lines:
+        for line in lines:
+            dictionary.append(line[:-1])
 
-# Build words
-outfile = "output.txt"
-f = codecs.open(outfile, "w", "utf-8")
+    # Load the trigram count matrix and normalize it (to be used as probability)
+    count = np.fromfile("count.bin", dtype="int32").reshape(256, 256, 256)
+    s = count.sum(axis=2)
+    st = np.tile(s.T, (256, 1, 1)).T
+    markov_chain = count.astype('float') / st
+    markov_chain[np.isnan(markov_chain)] = 0
 
-already_generated = []
+    # Build words
+    outfile = "output.txt"
+    f = codecs.open(outfile, "w", "utf-8")
 
-# How many words for each target size
-nb_words = 100
-for word_size in range(4, 11):
-    generated_words = 0
-    while generated_words < nb_words:
-        i = 0
-        j = 0
-        unicode_word = u''
-        while not j == 10:
-            unicode_code = choice(range(256), 1, p=markov_chain[i, j, :])[0]
-            unicode_word = unicode_word + chr(unicode_code)
-            i = j
-            j = unicode_code
-        if len(unicode_word) == 1 + word_size and unicode_word[:-1] not in already_generated:
-            # if the word exists in the dictionary, suffix it by *
-            if unicode_word[:-1] in dictionary:
-                final_word = unicode_word[:-1] + "*"
-            else:
-                final_word = unicode_word[:-1]
-            generated_words += 1
-            already_generated.append(unicode_word[:-1])
-            print(final_word)
-            f.write(final_word + "\n")
-f.close()
+    already_generated = []
+
+    # How many words for each target size
+    nb_words = 100
+    for word_size in range(4, 11):
+        generated_words = 0
+        while generated_words < nb_words:
+            i = 0
+            j = 0
+            unicode_word = u''
+            while not j == 10:
+                unicode_code = choice(range(256), 1, p=markov_chain[i, j, :])[0]
+                unicode_word = unicode_word + chr(unicode_code)
+                i = j
+                j = unicode_code
+            if len(unicode_word) == 1 + word_size and unicode_word[:-1] not in already_generated:
+                # if the word exists in the dictionary, suffix it by *
+                if unicode_word[:-1] in dictionary:
+                    final_word = unicode_word[:-1] + "*"
+                else:
+                    final_word = unicode_word[:-1]
+                generated_words += 1
+                already_generated.append(unicode_word[:-1])
+                print(final_word)
+                f.write(final_word + "\n")
+    f.close()
+
+
+if __name__ == '__main__':
+    run()
