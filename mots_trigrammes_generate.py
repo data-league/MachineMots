058cf67cfc99070cd5d6e7fb16a4a17460a2ea70
hyagoro <steve.gabet@gmail.com>
2022-01-27 13:09:50 +0100

    (upgrade) extract vars

--- mots_trigrammes_generate.py
+++ mots_trigrammes_generate.py
@@ -15,15 +15,22 @@ np.random.seed(42)
 def run():
     # Build a dictionary to check whether word already exists
     filepath = "names.txt"
+    # How many words for each target size
+    nb_words = 100
+    min_word_size = 4
+    max_word_size = 11
+    # fixed matrix size (from count.bin)
+    matrix_size = 256
+
     dictionary = []
     with codecs.open(filepath, "r", "utf-8") as lines:
         for line in lines:
             dictionary.append(line[:-1])
 
     # Load the trigram count matrix and normalize it (to be used as probability)
-    count = np.fromfile("count.bin", dtype="int32").reshape(256, 256, 256)
+    count = np.fromfile("count.bin", dtype="int32").reshape(matrix_size, matrix_size, matrix_size)
     s = count.sum(axis=2)
-    st = np.tile(s.T, (256, 1, 1)).T
+    st = np.tile(s.T, (matrix_size, 1, 1)).T
     markov_chain = count.astype('float') / st
     markov_chain[np.isnan(markov_chain)] = 0
 
@@ -33,16 +40,14 @@ def run():
 
     already_generated = []
 
-    # How many words for each target size
-    nb_words = 100
-    for word_size in range(4, 11):
+    for word_size in range(min_word_size, max_word_size):
         generated_words = 0
         while generated_words < nb_words:
             i = 0
             j = 0
             unicode_word = u''
             while not j == 10:
-                unicode_code = choice(range(256), 1, p=markov_chain[i, j, :])[0]
+                unicode_code = choice(range(matrix_size), 1, p=markov_chain[i, j, :])[0]
                 unicode_word = unicode_word + chr(unicode_code)
                 i = j
                 j = unicode_code
